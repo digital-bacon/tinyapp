cda829e2ab03d2c8e6dcf1092bc255ce55cd0e8a
Mark Bieganek <mark.bieganek@telus.com>
2022-11-30 12:56:18 -0700

    Return on all responses

--- express_server.js
+++ express_server.js
@@ -22,30 +22,31 @@ const PORT = 8080;
 app.set('view engine', 'ejs');
 
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static("public"));
 app.use(morgan('dev'));
+app.use(express.static("public"));
 app.use(cookieSession({
   name: 'session',
   keys: ['key1', 'key2'],
-
+  
   // Cookie Options
   maxAge: 24 * 60 * 60 * 1000 // 24 hours
 }));
 
 const authenticateUser = (email, password, datasetUser) => {
   if (validEmail(email) === false || validEmail(password) === false) return false;
-
+  
   // Find a user record that matches the provided email and password
   const userObject = Object.values(datasetUser)
     .find(userId => userId.email === email &&
       bcrypt.compareSync(password, userId.password)
-    );
-  
+      );
+
   // If a user was found, then this user is authenticated
   const isauthenticateUser = userObject !== undefined;
   return isauthenticateUser;
 };
 
+  
 
 /*
  * ROUTES FOR GET REQUESTS
@@ -53,92 +54,92 @@ const authenticateUser = (email, password, datasetUser) => {
 app.get('/u/:id', (req, res) => {
   const urlId = req.params.id;
   if (existsUrlId(urlId, dbUrl) === false) {
-    res.status(404).send('404 - Not found');
+    return res.status(404).send('404 - Not found');
   }
 
   const longUrl = dbUrl[urlId].longUrl;
-  res.redirect(longUrl);
+  return res.redirect(longUrl);
 });
 
 app.get('/urls/new', (req, res) => {
   // Redirect if the user is not logged in
   const userId = req.session.userId;
   if (loggedIn(userId, dbUser) === false) {
-    res.redirect('/login');
+    return res.redirect('/login');
   }
 
   const userData = getUserById(userId, dbUser);
   const templateVars = { userData };
-  res.render('urls_new', templateVars);
+  return res.render('urls_new', templateVars);
 });
 
 app.get('/urls/:id', (req, res) => {
   const urlId = req.params.id;
   // Respond with a 404 if the requested ID does not exist
   if (existsUrlId(urlId, dbUrl) === false) {
-    res.status(404).send('404 - Not found');
+    return res.status(404).send('404 - Not found');
   }
 
   const userId = req.session.userId;
   // Ensure user owns this urlId
   if (ownsUrlId(urlId, userId, dbUser, dbUrl) === false) {
-    res.status(403).send('403 - Forbidden. We could not authenticate you with the provided credentials.');
+    return res.status(403).send('403 - Forbidden. We could not authenticate you with the provided credentials.');
   }
 
   const longUrl = dbUrl[urlId].longUrl;
   const userData = getUserById(userId, dbUser);
   const templateVars = { userData, urlId, longUrl };
-  res.render('urls_show', templateVars);
+  return res.render('urls_show', templateVars);
 });
 
 app.get('/login', (req, res) => {
   // Redirect if the user is already logged in
   const userId = req.session.userId;
   if (loggedIn(userId, dbUser) === true) {
-    res.redirect('/urls');
+    return res.redirect('/urls');
   }
 
   const userData = getUserById(userId, dbUser);
   const templateVars = { userData };
-  res.render('login', templateVars);
+  return res.render('login', templateVars);
 });
 
 app.get('/logout', (req, res) => {
   req.session = null;
-  res.redirect('/login');
+  return res.redirect('/login');
 });
 
 app.get('/register', (req, res) => {
   // Redirect if the user is already logged in
   const userId = req.session.userId;
   if (loggedIn(userId, dbUser) === true) {
-    res.redirect('/urls');
+    return res.redirect('/urls');
   }
 
   const userData = getUserById(userId, dbUser);
   const templateVars = { userData };
-  res.render('register', templateVars);
+  return res.render('register', templateVars);
 });
 
 app.get('/urls', (req, res) => {
   // Redirect if the user is not logged in
   const userId = req.session.userId;
   if (loggedIn(userId, dbUser) === false) {
-    res.redirect('/login');
+    return res.redirect('/login');
   }
 
   const userData = getUserById(userId, dbUser);
   const urls = getUrlsByUserId(userId, dbUser, dbUrl);
   const templateVars = { userData, urls };
-  res.render('urls_index', templateVars);
+  return res.render('urls_index', templateVars);
 });
 
 app.get('/', (req, res) => {
-  res.redirect('/urls');
+  return res.redirect('/urls');
 });
 
 app.get('/*', (req, res) => {
-  res.status(404).send('404 - Not found');
+  return res.status(404).send('404 - Not found');
 });
 
 /*
@@ -148,90 +149,90 @@ app.post('/urls/:id/update', (req, res) => {
   // Redirect if the user is not logged in
   const userId = req.session.userId;
   if (loggedIn(userId, dbUser) === false) {
-    res.redirect('/login');
+    return res.redirect('/login');
   }
 
   const urlId = req.params.id;
   // Ensure the requested urlId exists
   if (existsUrlId(urlId, dbUrl) === false) {
-    res.status(404).send('404 - Not found');
+    return res.status(404).send('404 - Not found');
   }
 
   // Ensure user owns this urlId
   if (ownsUrlId(urlId, userId, dbUser, dbUrl) === false) {
-    res.status(403).send('403 - Forbidden. We could not authenticate you with the provided credentials.');
+    return res.status(403).send('403 - Forbidden. We could not authenticate you with the provided credentials.');
   }
 
   const submittedUrl = req.body.longUrl;
   dbUrl[urlId].longUrl = submittedUrl;
-  res.redirect('/urls');
+  return res.redirect('/urls');
 });
 
 app.post('/urls/:id/delete', (req, res) => {
   // Redirect if the user is not logged in
   const userId = req.session.userId;
   if (loggedIn(userId, dbUser) === false) {
-    res.redirect('/login');
+    return res.redirect('/login');
   }
 
   const urlId = req.params.id;
   // Ensure the requested urlId exists
   if (existsUrlId(urlId, dbUrl) === false) {
-    res.status(404).send('404 - Not found');
+    return res.status(404).send('404 - Not found');
   }
 
   // Ensure user owns this urlId
   if (ownsUrlId(urlId, userId, dbUser, dbUrl) === false) {
-    res.status(403).send('403 - Forbidden. We could not authenticate you with the provided credentials.');
+    return res.status(403).send('403 - Forbidden. We could not authenticate you with the provided credentials.');
   }
 
   delete dbUrl[urlId];
-  res.redirect('/urls');
+  return res.redirect('/urls');
 });
 
 app.post('/login', (req, res) => {
   // Redirect if the user is already logged in
   const userId = req.session.userId;
   if (loggedIn(userId, dbUser) === true) {
-    res.redirect('/urls');
+    return res.redirect('/urls');
   }
 
   const submittedEmail = req.body.email;
   const submittedPassword = req.body.password;
   // Don't allow a user to login if they didn't enter an email or password
   if (validEmail(submittedEmail) === false || validPassword(submittedPassword) === false) {
-    res.status(404).send('404 - Not found');
+    return res.status(404).send('404 - Not found');
   }
 
   // Authenticate the user
   if (authenticateUser(submittedEmail, submittedPassword, dbUser) === false) {
-    res.status(403).send('403 - Forbidden. We could not authenticate you with the provided credentials.');
+    return res.status(403).send('403 - Forbidden. We could not authenticate you with the provided credentials.');
   }
 
   // User was authenticated, retrieve user data
   const userData = getUserByEmail(submittedEmail, dbUser);
   // Log the user in, then redirect
   req.session.userId = userData.id;
-  res.redirect('/urls');
+  return res.redirect('/urls');
 });
 
 app.post('/register', (req, res) => {
   // Redirect if the user is already logged in
   const userId = req.session.userId;
   if (loggedIn(userId, dbUser) === true) {
-    res.redirect('/urls');
+    return res.redirect('/urls');
   }
 
   const submittedEmail = req.body.email;
   const submittedPassword = req.body.password;
   // Don't allow a user to register if they didn't enter an email or password
   if (validEmail(submittedEmail) === false || validPassword(submittedPassword) === false) {
-    res.status(404).send('404 - Not found');
+    return res.status(404).send('404 - Not found');
   }
   
   // Don't allow a user to register if they already have an account
   if (getUserByEmail(submittedEmail, dbUser) !== undefined) {
-    res.status(404).send('404 - Not found');
+    return res.status(404).send('404 - Not found');
   }
   
   // Hash the password
@@ -252,14 +253,14 @@ app.post('/register', (req, res) => {
 
   // Log the new user in, then redirect
   req.session.userId = newUserId;
-  res.redirect('/urls');
+  return res.redirect('/urls');
 });
 
 app.post('/urls', (req, res) => {
   // Redirect if the user is not logged in
   const userId = req.session.userId;
   if (loggedIn(userId, dbUser) === false) {
-    res.redirect('/login');
+    return res.redirect('/login');
   }
 
   // TODO: Validate submitted Url as a Url, handle response if invalid
@@ -274,7 +275,7 @@ app.post('/urls', (req, res) => {
   const useCharacters = Object.values(characterSets).join('');
   const newId = generateRandomString(6, useCharacters);
   dbUrl[newId] = { userId, longUrl: submittedUrl };
-  res.redirect('/urls');
+  return res.redirect('/urls');
 });
 
 app.listen(PORT, () => {
