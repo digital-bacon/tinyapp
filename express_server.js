eb99bd1c88d6af2dfd9ed42dba33c0c421e60e41
Mark Bieganek <mark.bieganek@telus.com>
2022-11-30 16:54:41 -0700

    Change Example app to TinyApp where found

--- express_server.js
+++ express_server.js
@@ -268,5 +268,5 @@ app.post('/urls', (req, res) => {
 });
 
 app.listen(PORT, () => {
-  console.log(`Example app listening on port ${PORT}`);
+  console.log(`TinyApp listening on port ${PORT}`);
 });
