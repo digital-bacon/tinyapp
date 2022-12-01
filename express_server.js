839b91257944436d1fcd43ce0c718cfbe4c05267
Mark Bieganek <mark.bieganek@telus.com>
2022-12-01 15:27:44 -0700

    Create middleware for authorizing users

--- express_server.js
+++ express_server.js
@@ -19,6 +19,12 @@ const PORT = 8080;
 
 app.set('view engine', 'ejs');
 
+/*
+ * MIDDLEWARE
+ */
+app.use(express.urlencoded({ extended: true }));
+app.use(morgan('dev'));
+app.use(express.static("public"));
 app.use(cookieSession({
   name: 'session',
   keys: [
@@ -33,9 +39,20 @@ app.use(cookieSession({
   maxAge: 24 * 60 * 60 * 1000 // 24 hours
 }));
 
-app.use(express.urlencoded({ extended: true }));
-app.use(morgan('dev'));
-app.use(express.static("public"));
+const authorize = (req, res, next) => {
+  let loggedIn = false;
+  const userId = req.session.userId;
+  if (userId !== undefined) {
+    const userData = filterUsers('id', userId, dbUser);
+    if (userData[userId] !== undefined) {
+      loggedIn = true;
+    }
+  }
+  if (loggedIn === false) {
+    return res.redirect('/login');
+  }
+  next();
+};
 
 /*
  * ROUTES FOR GET REQUESTS
@@ -67,27 +84,17 @@ app.get('/urls/new', (req, res) => {
   return res.render('urls_new', templateVars);
 });
 
-app.get('/urls', (req, res) => {
-  // Authorize user, redirect if not logged in
+app.get('/urls', authorize, (req, res) => {
   const userId = req.session.userId;
   const userData = filterUsers('id', userId, dbUser);
-  if (userData[userId] === undefined) {
-    return res.redirect('/login');
-  }
-
   const urls = filterUrls('userId', userId, dbUrl);
   const templateVars = { urls, userData: userData[userId] };
   return res.render('urls_index', templateVars);
 });
 
-app.get('/urls/:id', (req, res) => {
-  // Authorize user, redirect if not logged in
+app.get('/urls/:id', authorize, (req, res) => {
   const userId = req.session.userId;
   const userData = filterUsers('id', userId, dbUser);
-  if (userData[userId] === undefined) {
-    return res.redirect('/login');
-  }
-
   const urlId = req.params.id;
   // Ensure the requested urlId exists
   const urlData = dbUrl[urlId];
@@ -105,14 +112,8 @@ app.get('/urls/:id', (req, res) => {
   return res.render('urls_show', templateVars);
 });
 
-app.get('/u/:id', (req, res) => {
-  // Authorize user, redirect if not logged in
+app.get('/u/:id', authorize, (req, res) => {
   const userId = req.session.userId;
-  const userData = filterUsers('id', userId, dbUser);
-  if (userData[userId] === undefined) {
-    return res.redirect('/login');
-  }
-
   const urlId = req.params.id;
   // Ensure the requested urlId exists
   const urlData = dbUrl[urlId];
@@ -201,19 +202,15 @@ app.post('/register', (req, res) => {
   if (newUser === undefined) {
     return res.status(500).send('500 - Internal Server Error');
   }
+  
   // Log the new user in, then redirect
   req.session.userId = newUser.id;
   return res.redirect('/urls');
 });
 
-app.post('/urls', (req, res) => {
-  // Authorize user, redirect if not logged in
+app.post('/urls', authorize, (req, res) => {
   const userId = req.session.userId;
-  const userData = filterUsers('id', userId, dbUser);
-  if (userData[userId] === undefined) {
-    return res.redirect('/login');
-  }
-
+  // Validate the submitted url
   const submittedUrl = req.body.longUrl;
   if (validUrl(submittedUrl) === false) {
     return res.status(400).send('400 - It seems you did not provide a valid url');
@@ -228,15 +225,14 @@ app.post('/urls', (req, res) => {
   return res.redirect('/urls');
 });
 
-app.listen(PORT, () => {
-  console.log(`TinyApp listening on port ${PORT}`);
-});
-
-app.post('/urls/:id/update', (req, res) => {
-  // TODO: validate url input as a url
+app.post('/urls/:id/update', authorize, (req, res) => { 
+  // Authorize user, redirect if not logged in
   const userId = req.session.userId;
-  if (filterUsers('id', userId, dbUser) === undefined) {
-    return res.redirect('/login');
+  
+  // Validate the submitted url
+  const submittedUrl = req.body.longUrl;
+  if (validUrl(submittedUrl) === false) {
+    return res.status(400).send('400 - It seems you did not provide a valid url');
   }
 
   const urlId = req.params.id;
@@ -245,25 +241,19 @@ app.post('/urls/:id/update', (req, res) => {
   if (urlData === undefined) {
     return res.status(404).send('404 - Not found');
   }
-
+  
   // Ensure user owns this urlId
   if (urlData.userId !== userId) {
     return res.status(403).send('403 - Forbidden. We could not authenticate you with the provided credentials.');
   }
-
-  const submittedUrl = req.body.longUrl;
+  
   dbUrl[urlId].longUrl = submittedUrl;
   return res.redirect('/urls');
 });
 
-app.post('/urls/:id/delete', (req, res) => {
+app.post('/urls/:id/delete', authorize, (req, res) => {
   // Authorize user, redirect if not logged in
   const userId = req.session.userId;
-  const userData = filterUsers('id', userId, dbUser);
-  if (userData[userId] === undefined) {
-    return res.redirect('/login');
-  }
-
   const urlId = req.params.id;
   // Ensure the requested urlId exists
   const urlData = dbUrl[urlId];
@@ -278,4 +268,8 @@ app.post('/urls/:id/delete', (req, res) => {
 
   delete dbUrl[urlId];
   return res.redirect('/urls');
+});
+
+app.listen(PORT, () => {
+  console.log(`TinyApp listening on port ${PORT}`);
 });
