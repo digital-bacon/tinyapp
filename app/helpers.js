6b4e771bd360bb80e125fff7ebe547a28b586e08
Mark Bieganek <mark.bieganek@telus.com>
2022-12-01 19:33:24 -0700

    Add JSDoc comments to helper functions

--- app/helpers.js
+++ app/helpers.js
@@ -1,9 +1,18 @@
+/**
+ * Function that adds a new tiny url record for a given user and url
+ * and returns the tiny url shortcode when successful
+ * @param {Object} datasetUrl - The dataset to add the record to
+ * @param {string} userId - The owner of the record
+ * @param {string} longUrl - The long form url that users will be
+ * redirected to if they request this tiny url
+ * @returns {string | undefined} The tiny url code
+ */
 const createTinyUrl = (datasetUrl, userId, longUrl) => {
   if (validUrl(longUrl) === false) return;
   if (typeof userId !== 'string') return;
   if (userId === '') return;
   if (typeof datasetUrl !== 'object' || Array.isArray(datasetUrl)) return;
-  const newUrlId = generateUrlId();
+  const newUrlId = generateUrlShortcode();
   datasetUrl[newUrlId] = {
     userId,
     longUrl
@@ -11,6 +20,15 @@ const createTinyUrl = (datasetUrl, userId, longUrl) => {
   return newUrlId;
 };
 
+/**
+ * Function that adds a new user record for a user when provided
+ * with an email address and hashed password. Returns the new user id
+ * when successful.
+ * @param {Object} datasetUser - The dataset to add the record to
+ * @param {string} email - The user's email address
+ * @param {string} password - The hashed password for this user
+ * @returns {string | undefined} The new user id
+ */
 const createUser = (datasetUser, email, password) => {
   if (validEmail === false || validPassword === false) return;
   if (typeof datasetUser !== 'object' || Array.isArray(datasetUser)) return;
@@ -24,7 +42,11 @@ const createUser = (datasetUser, email, password) => {
   return id;
 };
 
-const generateUrlId = () => {
+/**
+ * Function that generates a random url identifier code
+ * @returns {string} The new url identifier code
+ */
+const generateUrlShortcode = () => {
   const characterSets = {
     lowercase: 'abcdefghijklmnopqrstuvwxyz',
     uppercase: 'ABCDEFGHIJKLMNOPQRSTUVWXYZ',
@@ -36,6 +58,10 @@ const generateUrlId = () => {
   return newId;
 };
 
+/**
+ * Function that generates a random user identifier code
+ * @returns {string} The new user identifier code
+ */
 const generateUserId = () => {
   const characterSets = {
     lowercase: 'abcdefghijklmnopqrstuvwxyz',
@@ -63,6 +89,13 @@ const generateRandomString = (desiredLength = 0, characterSet) => {
   return randomString;
 };
 
+/**
+ * Function that filters a given url dataset for a single key and value
+ * @param {string} key - The property key to match
+ * @param {string} value - The property value to match
+ * @param {Object} datasetUrl - The dataset from which to look
+ * @returns {Object} The filtered results
+ */
 const filterUrls = (key, value, datasetUrl) => {
   const urlData = {};
   if (typeof key !== 'string') return urlData;
@@ -79,6 +112,13 @@ const filterUrls = (key, value, datasetUrl) => {
   return urlData;
 };
 
+/**
+ * Function that filters a given user dataset for a single key and value
+ * @param {string} key - The property key to match
+ * @param {string} value - The property value to match
+ * @param {Object} datasetUser - The dataset from which to look
+ * @returns {Object} The filtered results
+ */
 const filterUsers = (key, value, datasetUser) => {
   const userData = {};
   if (typeof key !== 'string') return userData;
@@ -96,6 +136,11 @@ const filterUsers = (key, value, datasetUser) => {
   return userData;
 };
 
+/**
+ * Function that validates a given email as an email address
+ * @param {string} email - The email to evaluate 
+ * @returns {boolean} When valid, returns true
+ */
 const validEmail = (email) => {
   if (typeof email !== 'string') return false;
   if (email === '') return false;
@@ -104,13 +149,24 @@ const validEmail = (email) => {
   return true;
 };
 
+/**
+ * Function that validates a given plaintext password
+ * @param {string} password - The plaintext password to evaluate 
+ * @returns {boolean} When valid, returns true
+ */
 const validPassword = (password) => {
   if (typeof password !== 'string') return false;
   if (password === '') return false;
   return true;
 };
 
+/**
+ * Function that validates a given url
+ * @param {string} password - The plaintext password to evaluate 
+ * @returns {boolean} When valid, returns true
+ */
 const validUrl = (url) => {
+  // TODO: Use regex to ensure the url includes http(s)://
   if (typeof url !== 'string') return false;
   if (url === '') return false;
   return true;
